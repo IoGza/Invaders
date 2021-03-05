60e67952eee522a3a8c604cf6c31fa8b5bb3821e
Evan McKinney-Larkin <evanmckinney07@gmail.com>
2021-03-05 13:23:09 -0600

    Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,37 +1,37 @@
-import sys
 import pygame
+import sys
+from settings import Settings
 
 # Class to manage ALien Invasion window and game assets
 class AlienInvasion:
     def __init__(self):
+
+        
         # initializes game and creates the resources
         pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 
         # Sets up the game window display and the display title/caption
         # Represented as a tuple giving the window its dimensions
         # A surface was assigned to the self.screen attribute, allowing
         # an element to be displayed on its own designated surface
-        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
-
-        # Sets the background color
-        self.bg_color = (230, 230, 230)
 
         # Main loop for the game
 
     def run_game(self):
-        while True:
+        while True: 
+
             # Keyboard and mouse inputs
-            # Returns a list of events that were preformed on the method event.get()
+            # Redraws the screen through each loop, giving it a color
+            self.screen.fill(self.settings.bg_color)
             for event in pygame.event.get():
-                # Exits the game if user chooses to quit the screen
                 if event.type == pygame.QUIT:
                     sys.exit()
-
-                # Redraws the screen through each loop, giving it a color
-                self.screen.fill(self.bg_color)
-                # Make recently drawn screen visible, erasing the old screen and drawing a new one
-                pygame.display.flip()
+            # Make recently drawn screen visible, erasing the old screen and drawing a new one
+            pygame.display.flip()
 
 
 if __name__ == "__main__":
---
+++ settings.py
@@ -0,0 +1,12 @@
+import pygame
+
+# Class which will store all of the game's settings 
+class Settings:
+
+    def __init__(self):
+
+        # Initializing game settings and screen settings
+
+        self.screen_width= 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
