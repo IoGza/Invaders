b22ed519b0d631b3603ec5f442b23416be1bdebf
Evan McKinney-Larkin <evanmckinney07@gmail.com>
2021-03-07 02:38:02 -0600

    bullets added

--- settings.py
+++ settings.py
@@ -11,3 +11,9 @@ class Settings:
         self.bg_color = (230,230,230)
         #Sets the speed of the ship movement 
         self.ship_speed = 1.5
+
+        #Values for the bullets the ship will shoot
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
